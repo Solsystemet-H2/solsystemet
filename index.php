6229976ae24c83225e8619479089c1ed000e0584
Kent Munch Jensen (zbckejen5) <zbckejen5@zbc.dk>
2018-02-20 13:02:23 +0100

    Css changes, added planet site

--- index.php
+++ index.php
@@ -29,6 +29,10 @@ $(function() {
     scrollInertia:100,
     mouseWheel:{ scrollAmount: 50 },
   });
+
+  $(".planet").on("click",function(){
+    window.location = "planet.php?id="+$(this).attr("id");
+  });
 });
 </script>
 </head>
@@ -48,16 +52,46 @@ $(function() {
     ?>
   </div>
   <div id="spaceContainer">
-    <div  id="sun" class="planet"><img src="images\planets\sun.png" /><div class="planetName">Solen</div></div>
-    <div id="mercury" class="planet" ><img src="images\planets\mercury.png" /><div class="planetName">Merkur</div></div>
-    <div id="venus" class="planet"><img src="images\planets\venus.png" /><div class="planetName">Venus</div></div>
-    <div id="earth" class="planet"><img src="images\planets\earth.png" /><div class="planetName">Jorden</div></div>
-    <div id="mars" class="planet"><img src="images\planets\mars.png" /><div class="planetName">Mars</div></div>
-    <div id="jupiter" class="planet"><img src="images\planets\jupiter.png" /><div class="planetName">Jupiter</div></div>
-    <div id="saturn" class="planet"><img src="images\planets\saturn.png" /><div class="planetName">Saturn</div></div>
-    <div id="uranus" class="planet"><img src="images\planets\uranus.png" /><div class="planetName">Uranus</div></div>
-    <div id="neptune" class="planet"><img src="images\planets\neptune.png" /><div class="planetName">Neptun</div></div>
-    <div id="pluto" class="planet"><img src="images\planets\pluto.png" /><div class="planetName">Pluto</div></div>
+    <div id="sun" class="planet">
+      <img title="Solen" alt="Solen" src="images\planets\sun.png" />
+      <div class="planetName">Solen</div>
+    </div>
+    <div id="mercury" class="planet" >
+      <img title="Merkur" alt="Merkur" src="images\planets\mercury.png" />
+      <div class="planetName">Merkur</div>
+    </div>
+    <div id="venus" class="planet">
+      <img title="Venus" alt="Venus" src="images\planets\venus.png" />
+      <div class="planetName">Venus</div>
+    </div>
+    <div id="earth" class="planet">
+      <img title="Jorden" alt="Jorden" src="images\planets\earth.png" />
+      <div class="planetName">Jorden</div>
+    </div>
+    <div id="mars" class="planet">
+      <img title="Mars" alt="Mars" src="images\planets\mars.png" />
+      <div class="planetName">Mars</div>
+    </div>
+    <div id="jupiter" class="planet">
+      <img title="Jupiter" alt="Jupiter" src="images\planets\jupiter.png" />
+      <div class="planetName">Jupiter</div>
+    </div>
+    <div id="saturn" class="planet">
+      <img title="Saturn" alt="Saturn" src="images\planets\saturn.png" />
+      <div class="planetName">Saturn</div>
+    </div>
+    <div id="uranus" class="planet">
+      <img title="Uranus" alt="Uranus" src="images\planets\uranus.png" />
+      <div class="planetName">Uranus</div>
+    </div>
+    <div id="neptune" class="planet">
+      <img title="Neptun" alt="Neptun" src="images\planets\neptune.png" />
+      <div class="planetName">Neptun</div>
+    </div>
+    <div id="pluto" class="planet">
+      <img title="Pluto" alt="Pluto" src="images\planets\pluto.png" />
+      <div class="planetName">Pluto</div>
+    </div>
     <div id="orbit1" class="planetOrbit"></div>
     <div id="orbit2" class="planetOrbit"></div>
     <div id="orbit3" class="planetOrbit"></div>
---
+++ planet.php
@@ -0,0 +1,179 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <title>Sol Systemet - Planet navn</title>
+
+    <link rel="stylesheet" href="planetStyle.css">
+
+    <script src="scripts/jquery-3.2.1.min.js"></script>
+
+    <link rel="stylesheet" href="scripts/bootstrap/css/bootstrap.min.css">
+    <script src="scripts\bootstrap\js\bootstrap.min.js"></script>
+
+    <link rel="stylesheet" href="scripts/mCustomScrollbar/mCustomScrollbar.min.css">
+    <script src="scripts\mCustomScrollbar\mCustomScrollbar.min.js"></script>
+
+    <link rel="stylesheet" href="scripts/font-awesome/css/font-awesome.min.css">
+
+    <script src="scripts\particles.js\particles.min.js"></script>
+
+
+<script>
+$(function() {
+  particlesJS.load('particles-js', 'scripts/particles.js/spaceBG.json', function() {});
+
+  $("#planetInfo").mCustomScrollbar({
+    scrollInertia:100,
+    mouseWheel:{ scrollAmount: 50 },
+  });
+});
+</script>
+</head>
+
+<body id="particles-js">
+  <div id="mainContainer">
+    <div class="container">
+      <div class="row">
+        <div id="planetImageContainer" class="col-md-6 col-sm-12 editable">
+          <img title="Planet Navn" alt="Planet Navn" id="planetImage" src="images/planetsRealistic/mars.png" />
+        </div>
+        <div id="planetInfoContainer" class="col-md-6 col-sm-12 editable">
+          <h1 id="planetName">Planet navn</h1>
+          <div id="planetInfo">
+            <?php
+              echo nl2br("
+              <strong>Lorem:</strong> Ipsum
+              <strong>Lorem:</strong> Ipsum
+              <strong>Lorem:</strong> Ipsum
+              <strong>Lorem:</strong> Ipsum
+              <strong>Lorem:</strong> Ipsum
+
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin in velit a magna molestie porta ac nec justo.
+              Suspendisse posuere accumsan nisi id pharetra. Sed augue purus, commodo et feugiat et,
+              mattis at est. Sed auctor metus sapien.
+              Vehicula nulla tempor id. Donec dignissim risus sit amet imperdiet pharetra. Vivamus quam elit,
+              finibus eget leo a, ornare laoreet diam. Mauris iaculis, libero sed hendrerit molestie,
+              neque velit lacinia nisl.
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin in velit a magna molestie porta ac nec justo.
+              Suspendisse posuere accumsan nisi id pharetra. Sed augue purus, commodo et feugiat et,
+              mattis at est. Sed auctor metus sapien.
+              Vehicula nulla tempor id. Donec dignissim risus sit amet imperdiet pharetra. Vivamus quam elit,
+              finibus eget leo a, ornare laoreet diam. Mauris iaculis, libero sed hendrerit molestie,
+              neque velit lacinia nisl.
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin in velit a magna molestie porta ac nec justo.
+              Suspendisse posuere accumsan nisi id pharetra. Sed augue purus, commodo et feugiat et,
+              mattis at est. Sed auctor metus sapien.
+              Vehicula nulla tempor id. Donec dignissim risus sit amet imperdiet pharetra. Vivamus quam elit,
+              finibus eget leo a, ornare laoreet diam. Mauris iaculis, libero sed hendrerit molestie,
+              neque velit lacinia nisl.
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin in velit a magna molestie porta ac nec justo.
+              Suspendisse posuere accumsan nisi id pharetra. Sed augue purus, commodo et feugiat et,
+              mattis at est. Sed auctor metus sapien.
+              Vehicula nulla tempor id. Donec dignissim risus sit amet imperdiet pharetra. Vivamus quam elit,
+              finibus eget leo a, ornare laoreet diam. Mauris iaculis, libero sed hendrerit molestie,
+              neque velit lacinia nisl.
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin in velit a magna molestie porta ac nec justo.
+              Suspendisse posuere accumsan nisi id pharetra. Sed augue purus, commodo et feugiat et,
+              mattis at est. Sed auctor metus sapien.
+              Vehicula nulla tempor id. Donec dignissim risus sit amet imperdiet pharetra. Vivamus quam elit,
+              finibus eget leo a, ornare laoreet diam. Mauris iaculis, libero sed hendrerit molestie,
+              neque velit lacinia nisl.
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin in velit a magna molestie porta ac nec justo.
+              Suspendisse posuere accumsan nisi id pharetra. Sed augue purus, commodo et feugiat et,
+              mattis at est. Sed auctor metus sapien.
+              Vehicula nulla tempor id. Donec dignissim risus sit amet imperdiet pharetra. Vivamus quam elit,
+              finibus eget leo a, ornare laoreet diam. Mauris iaculis, libero sed hendrerit molestie,
+              neque velit lacinia nisl.
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin in velit a magna molestie porta ac nec justo.
+              Suspendisse posuere accumsan nisi id pharetra. Sed augue purus, commodo et feugiat et,
+              mattis at est. Sed auctor metus sapien.
+              Vehicula nulla tempor id. Donec dignissim risus sit amet imperdiet pharetra. Vivamus quam elit,
+              finibus eget leo a, ornare laoreet diam. Mauris iaculis, libero sed hendrerit molestie,
+              neque velit lacinia nisl.
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin in velit a magna molestie porta ac nec justo.
+              Suspendisse posuere accumsan nisi id pharetra. Sed augue purus, commodo et feugiat et,
+              mattis at est. Sed auctor metus sapien.
+              Vehicula nulla tempor id. Donec dignissim risus sit amet imperdiet pharetra. Vivamus quam elit,
+              finibus eget leo a, ornare laoreet diam. Mauris iaculis, libero sed hendrerit molestie,
+              neque velit lacinia nisl.
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin in velit a magna molestie porta ac nec justo.
+              Suspendisse posuere accumsan nisi id pharetra. Sed augue purus, commodo et feugiat et,
+              mattis at est. Sed auctor metus sapien.
+              Vehicula nulla tempor id. Donec dignissim risus sit amet imperdiet pharetra. Vivamus quam elit,
+              finibus eget leo a, ornare laoreet diam. Mauris iaculis, libero sed hendrerit molestie,
+              neque velit lacinia nisl.
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin in velit a magna molestie porta ac nec justo.
+              Suspendisse posuere accumsan nisi id pharetra. Sed augue purus, commodo et feugiat et,
+              mattis at est. Sed auctor metus sapien.
+              Vehicula nulla tempor id. Donec dignissim risus sit amet imperdiet pharetra. Vivamus quam elit,
+              finibus eget leo a, ornare laoreet diam. Mauris iaculis, libero sed hendrerit molestie,
+              neque velit lacinia nisl.
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin in velit a magna molestie porta ac nec justo.
+              Suspendisse posuere accumsan nisi id pharetra. Sed augue purus, commodo et feugiat et,
+              mattis at est. Sed auctor metus sapien.
+              Vehicula nulla tempor id. Donec dignissim risus sit amet imperdiet pharetra. Vivamus quam elit,
+              finibus eget leo a, ornare laoreet diam. Mauris iaculis, libero sed hendrerit molestie,
+              neque velit lacinia nisl.
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin in velit a magna molestie porta ac nec justo.
+              Suspendisse posuere accumsan nisi id pharetra. Sed augue purus, commodo et feugiat et,
+              mattis at est. Sed auctor metus sapien.
+              Vehicula nulla tempor id. Donec dignissim risus sit amet imperdiet pharetra. Vivamus quam elit,
+              finibus eget leo a, ornare laoreet diam. Mauris iaculis, libero sed hendrerit molestie,
+              neque velit lacinia nisl.
+
+              Fusce nibh sapien, imperdiet at neque a, mollis euismod mi.");
+            ?>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+  <div id="planetMenu">
+    <div id="sun" class="planet">
+      <img title="Solen" alt="Solen" src="images\planets\sun.png" />
+      <div class="planetName">Solen</div>
+    </div>
+    <div id="mercury" class="planet" >
+      <img title="Merkur" alt="Merkur" src="images\planets\mercury.png" />
+      <div class="planetName">Merkur</div>
+    </div>
+    <div id="venus" class="planet">
+      <img title="Venus" alt="Venus" src="images\planets\venus.png" />
+      <div class="planetName">Venus</div>
+    </div>
+    <div id="earth" class="planet">
+      <img title="Jorden" alt="Jorden" src="images\planets\earth.png" />
+      <div class="planetName">Jorden</div>
+    </div>
+    <div id="mars" class="planet">
+      <img title="Mars" alt="Mars" src="images\planets\mars.png" />
+      <div class="planetName">Mars</div>
+    </div>
+    <div id="jupiter" class="planet">
+      <img title="Jupiter" alt="Jupiter" src="images\planets\jupiter.png" />
+      <div class="planetName">Jupiter</div>
+    </div>
+    <div id="saturn" class="planet">
+      <img title="Saturn" alt="Saturn" src="images\planets\saturn.png" />
+      <div class="planetName">Saturn</div>
+    </div>
+    <div id="uranus" class="planet">
+      <img title="Uranus" alt="Uranus" src="images\planets\uranus.png" />
+      <div class="planetName">Uranus</div>
+    </div>
+    <div id="neptune" class="planet">
+      <img title="Neptun" alt="Neptun" src="images\planets\neptune.png" />
+      <div class="planetName">Neptun</div>
+    </div>
+    <div id="pluto" class="planet">
+      <img title="Pluto" alt="Pluto" src="images\planets\pluto.png" />
+      <div class="planetName">Pluto</div>
+    </div>
+  </div>
+</body>
+
+</html>
