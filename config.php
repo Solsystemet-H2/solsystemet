d247d24258acc14043ec16c9659c95caf4cb6cc8
Kent Munch Jensen (zbckejen5) <zbckejen5@zbc.dk>
2018-02-26 11:44:21 +0100

    added coments in config.php

--- config.php
+++ config.php
@@ -1,55 +1,66 @@
 <?php
+//Create a connection to the database
+//Requires a host (ip), username, password and the database name
 function dbConnect($host, $user, $pass, $database){
-  $mysqli=mysqli_connect($host, $user, $pass, $database);
-  $mysqli->set_charset('utf8');
+  $mysqli=mysqli_connect($host, $user, $pass, $database); //Connect the database using the values the user defined
+  $mysqli->set_charset('utf8'); //Set the charset to UTF-8 so we won't hve any issues with special characters
 
-  if ($mysqli->connect_errno) {
-      printf("Connect failed: %s\n", $mysqli->connect_error);
-      exit();
+  if ($mysqli->connect_errno) { //If the connection encounters an error
+      printf("Connect failed: %s\n", $mysqli->connect_error); //Write the error
+      exit(); //Exit the function
   }
 
-  return $mysqli;
+  return $mysqli; //Return the connection
 }
 
+//Close the connection to the database
+//Requires an connection
 function dbDisconnect($mysqli){
-  mysqli_close($mysqli);
+  mysqli_close($mysqli); //Close the connection defined by the user
 }
 
+//Select one or multiple rows from the database
+//Requires: Connection to the database, database table, what we want to select,
+//from where we want it, WHERE condition and with a field reference, LIMIT condition,
+//ORDERBY condition with field and type (ASC or DESC) and if it you want one or multiple reqults
 function selectRow($mysqli, $table, $what, $where, $field, $limit, $orderby, $orderType, $multiple){
+  //Defines global function variables
   $insertWhere = "";
   $insertLimit = "";
   $insertOrder = "";
 
-  if($where != "" && $field != ""){
-    $insertWhere = "WHERE LOWER($where) = LOWER('$field')";
+  if($where != "" && $field != ""){ //If $where and $field isn't empty
+    $insertWhere = "WHERE LOWER($where) = LOWER('$field')"; //Updates the $insertWhere variable to contain the WHERE condition
   }
-  if($limit != ""){
-    $insertLimit = "LIMIT $limit";
+  if($limit != ""){ //If $limit isn't empty
   }
-  if($orderby != "" && $orderType != ""){
-    $insertOrder = "ORDER BY ".$orderby." ".strtoupper($orderType);
+  $insertLimit = "LIMIT $limit"; //Updates the $insertLimit variable to contain the LIMIT condition
+  if($orderby != "" && $orderType != ""){ //If $orderby and $orderType isn't empty
+    $insertOrder = "ORDER BY ".$orderby." ".strtoupper($orderType); //Updates the $insertOrder variable to contain the ORDER BY condition
   }
 
-  $query = "SELECT $what FROM $table $insertWhere $insertOrder $insertLimit";
-  $result = $mysqli->query($query);
+  $query = "SELECT $what FROM $table $insertWhere $insertOrder $insertLimit"; //Construct the sql query
+  $result = $mysqli->query($query); //Convert it to an sql element
 
-  if ($multiple == true) {
-    return $result;
+  if ($multiple == true) { //If the variable $multiple is true
+    return $result; //Return the $result
   }else{
-    $row = $result->fetch_array(MYSQLI_ASSOC);
-    return $row;
+    $row = $result->fetch_array(MYSQLI_ASSOC); //Fetches the row from the database
+    return $row; //Return row
   }
 }
 
+//Generate the planet menu used on planet.php
+//Requires an mysqli result
 function generatePlanetMenu($mysqliResult){
   ?>
   <div id="planetMenu">
     <?php
-    while($row = $mysqliResult->fetch_array(MYSQLI_ASSOC)){
+    while($row = $mysqliResult->fetch_array(MYSQLI_ASSOC)){ //While there is a row available to fetch, take and put into the variable $row
       ?>
-      <div id="<?php echo $row["StyleID"];?>" class="planet" onclick="window.location = 'planet.php?id=<?php echo $row["ID"];?>'">
-      <img title="<?php echo $row["Name"];?>" alt="<?php echo $row["Name"];?>" src="<?php echo $row["CartoonImage"];?>" />
-      <div class="planetName"><?php echo $row["Name"];?></div>
+      <div id="<?php echo $row["StyleID"]; //Get the id from the database ?>" class="planet" onclick="window.location = 'planet.php?id=<?php echo $row["ID"]; //Get the planet ID from the database ?>'">
+      <img title="<?php echo $row["Name"]; //Get the name from the database?>" alt="<?php echo $row["Name"]; //Get the name from the database ?>" src="<?php echo $row["CartoonImage"]; //Get the cartoon image url from the database ?>" />
+      <div class="planetName"><?php echo $row["Name"]; //Get the name from the database ?></div>
       </div>
       <?php
     }
@@ -58,16 +69,17 @@ function generatePlanetMenu($mysqliResult){
   <?php
 }
 
-
+//Generate the planet menu used on index.php
+//Requires an mysqli result
 function generatePlanetMenuFpage($mysqliResult){
   ?>
   <div id="responsivePlanetsContainer">
     <?php
-    while($row = $mysqliResult->fetch_array(MYSQLI_ASSOC)){
+    while($row = $mysqliResult->fetch_array(MYSQLI_ASSOC)){ //While there is a row available to fetch, take and put into the variable $row
       ?>
-      <div id="<?php echo $row["StyleID"];?>Responsive" class="responsivePlanet" onclick="window.location = 'planet.php?id=<?php echo $row["ID"];?>'">
-      <img title="<?php echo $row["Name"];?>" alt="<?php echo $row["Name"];?>" src="<?php echo $row["CartoonImage"];?>" />
-      <div class="responsivePlanetName"><?php echo $row["Name"];?></div>
+      <div id="<?php echo $row["StyleID"]; //Get the StyleID from the database ?>Responsive" class="responsivePlanet" onclick="window.location = 'planet.php?id=<?php echo $row["ID"]; //Get the id from the database ?>'">
+      <img title="<?php echo $row["Name"]; //Get the name from the database ?>" alt="<?php echo $row["Name"]; //Get the name from the database ?>" src="<?php echo $row["CartoonImage"]; //Get the cartoon image url from the database ?>" />
+      <div class="responsivePlanetName"><?php echo $row["Name"]; //Get the name from the database ?></div>
       </div>
       <?php
     }
@@ -76,43 +88,45 @@ function generatePlanetMenuFpage($mysqliResult){
   <?php
 }
 
+//Generate the solar system on the index.php
+//Requires an mysqli result
 function generateFrontPage($mysqliResult){
   ?>
   <div id="mainSpaceContainer">
   <div id="spaceContainer">
   <?php
   $i = 0;
-  while($row = $mysqliResult->fetch_array(MYSQLI_ASSOC)){
-    if($row["PlanetsOrder"] == 1){
+  while($row = $mysqliResult->fetch_array(MYSQLI_ASSOC)){ //While there is a row available to fetch, take and put into the variable $row
+    if($row["PlanetsOrder"] == 1){ //Seperate the sun because it dosen't have the same html markup as the rest
       $i++;
       ?>
-      <div id="<?php echo $row["StyleID"];?>" planetOrder="<?php echo $row["PlanetsOrder"];?>" class="planet">
-        <img title="<?php echo $row["Name"];?>" alt="<?php echo $row["Name"];?>" src="<?php echo $row["CartoonImage"];?>" />
-        <div class="planetName"><?php echo $row["Name"];?></div>
+      <div id="<?php echo $row["StyleID"]; //Get the styleID from the database ?>" planetOrder="<?php echo $row["PlanetsOrder"]; //Get the planet order in the solar system from the database ?>" class="planet">
+        <img title="<?php echo $row["Name"]; //Get the name from the database ?>" alt="<?php echo $row["Name"]; //Get the name from the database ?>" src="<?php echo $row["CartoonImage"]; //Get the cartoon image url from the database ?>" />
+        <div class="planetName"><?php echo $row["Name"]; //Get the name from the database ?></div>
       </div>
       <?php
-    }else if($row["PlanetsOrder"] == 7){
+    }else if($row["PlanetsOrder"] == 7){ //Seperate the saturn because it dosen't have the same html markup as the rest
       $i++;
       ?>
-      <div id="orbit<?php echo $i-1; ?>" class="planetOrbit">
+      <div id="orbit<?php echo $i-1; //Sets the planet orbit order ?>" class="planetOrbit">
         <div class="pos">
-          <div id="saturnShadow" planetOrder="<?php echo $i; ?>" class="planet"></div>
-          <div id="<?php echo $row["StyleID"];?>" planetOrder="<?php echo $row["PlanetsOrder"];?>" class="planet">
-            <img title="<?php echo $row["Name"];?>" alt="<?php echo $row["Name"];?>" src="<?php echo $row["CartoonImage"];?>" />
-            <div class="planetName"><?php echo $row["Name"];?></div>
+          <div id="saturnShadow" planetOrder="<?php echo $i; //Sets the planet order ?>" class="planet"></div>
+          <div id="<?php echo $row["StyleID"]; //Get the styleID from the database ?>" planetOrder="<?php echo $row["PlanetsOrder"]; //Sets the planet order ?>" class="planet">
+            <img title="<?php echo $row["Name"]; //Get the name from the database ?>" alt="<?php echo $row["Name"];  //Get the name from the database ?>" src="<?php echo $row["CartoonImage"]; //Get the cartoon image url from the database ?>" />
+            <div class="planetName"><?php echo $row["Name"]; //Get the name from the database ?></div>
           </div>
         </div>
       </div>
       <?php
-    }else{
+    }else{ //If any other planet than the two previously defined
       $i++;
       ?>
-      <div id="orbit<?php echo $i-1; ?>" class="planetOrbit">
+      <div id="orbit<?php echo $i-1; //Sets the planet orbit order ?>" class="planetOrbit">
         <div class="pos">
-          <div id="<?php echo $row["StyleID"];?>" planetOrder="<?php echo $row["PlanetsOrder"];?>" class="planet">
+          <div id="<?php echo $row["StyleID"]; //Get the styleID from the database ?>" planetOrder="<?php echo $row["PlanetsOrder"]; //Sets the planet order ?>" class="planet">
             <div class="planetShadow"></div>
-            <img title="<?php echo $row["Name"];?>" alt="<?php echo $row["Name"];?>" src="<?php echo $row["CartoonImage"];?>" />
-            <div class="planetName"><?php echo $row["Name"];?></div>
+            <img title="<?php echo $row["Name"]; //Get the name from the database ?>" alt="<?php echo $row["Name"]; //Get the name from the database ?>" src="<?php echo $row["CartoonImage"]; //Get the cartoon image url from the database ?>" />
+            <div class="planetName"><?php echo $row["Name"]; //Get the name from the database ?></div>
           </div>
         </div>
       </div>
